017c992d241095927c0e69c96217b50439f0cc22
villafrancanjelo <jelo.villafranca@gmail.com>
2022-01-07 10:25:07 +0800

    content

--- calculator/src/app/app.component.ts
+++ calculator/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+
+  public value1!:number;
+public value2!:number;
+public value3!:number;
+
+add(){
+  this.value3=this.value1+this.value2;
 }
+sub(){
+  this.value3=this.value1-this.value2;
+}
+multiply(){
+  this.value3=this.value1*this.value2;
+}
+divide(){
+  this.value3=this.value1/this.value2;
+}
+}
+
